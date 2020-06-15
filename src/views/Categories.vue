5c390de1029c6976be6ce1d86c019050434e9df1
Arnulfo L <arnulfo.lg@live.com>
2020-06-15 17:59:33 -0500

    Added myplaces and categories pages ans some styling

---
+++ src/views/Categories.vue
@@ -0,0 +1,32 @@
+<template>
+	<section class="categories">
+		<h2>Categories</h2>
+		<p>
+			Categories define what kind of place you are visiting. It stablishes a
+			relationship between places so you can plan accordingly.
+		</p>
+		<section class="places">
+			<AppCard v-for="i in 7" :key="i"></AppCard>
+		</section>
+	</section>
+</template>
+
+<script>
+import AppCard from "@/components/AppCard.vue"
+
+export default {
+	name: "App",
+	components: {
+		AppCard
+	}
+}
+</script>
+
+<style lang="scss">
+.categories .places {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-auto-rows: auto;
+	grid-gap: 12px;
+}
+</style>
---
+++ src/views/MyPlaces.vue
@@ -0,0 +1,32 @@
+<template>
+	<section class="myplaces">
+		<h2>My places</h2>
+		<p>
+			A list of all the places you have visited and the places you want to
+			visit.
+		</p>
+		<section class="places">
+			<AppCard v-for="i in 5" :key="i"></AppCard>
+		</section>
+	</section>
+</template>
+
+<script>
+import AppCard from "@/components/AppCard.vue"
+
+export default {
+	name: "App",
+	components: {
+		AppCard
+	}
+}
+</script>
+
+<style lang="scss">
+.myplaces .places {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-auto-rows: auto;
+	grid-gap: 12px;
+}
+</style>
